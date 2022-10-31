434742953eaf2e4d5fde616b14016d766e6aa4bb
Barzin <barzin.development@gmail.com>
2022-10-31 14:43:18 +0100

    Refactored files and folders so i can follow the Winc basic react lessons.

--- my-todo-list/src/components/App.js
+++ my-todo-list/src/components/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import './App.css';
+/*
+Time to have fun styling! But first things first: 
 
+1. Change the input/p combo below to be a new component called <TodoItem />. <TodoItem /> (for now) will just have the same displayed data below (every todo item is the same) hardcoded inside of it. (We'll learn soon how to make the TodoItem more flexible)
+    
+2. Style up the page however you want! You're welcome to use regular CSS (in the CSS file) or inline styles, or both!
+*/
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import React from "react"
+
+const App = () => {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+            
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+            
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+            
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
