44884080d2662ae3152ac7c665a4e6519e9bdb3a
Barzin <barzin.development@gmail.com>
2022-10-31 17:32:49 +0100

    Add changes based on lesson 17: React Inline Styles with the Style Property.

---
+++ my-todo-list/src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const TodoItem = () =>{
+    return (
+        <div style={{color:'rgba(12, 40, 78, 0.973)'}} className="todo-item">
+            <input type="checkbox" />
+            <p>Placeholder text here</p>
+        </div>
+
+    )
+}
+
+export default TodoItem
