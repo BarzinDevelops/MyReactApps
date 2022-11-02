1c78eebac1da6644e1b35578fe68fe0064b12dd4
Barzin <barzin.development@gmail.com>
2022-11-02 15:40:53 +0100

    React - Lesson 24 | Mapping Components Practice. Create new react app 'products-mapping'.

---
+++ products-mapping/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Products from './Products';
+import productData from './myProductsData'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Products />
+    </div>
+  );
+}
+
+export default App;
