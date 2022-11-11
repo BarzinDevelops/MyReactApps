d333d152f43779ad75bf0c73324d48a0ba68f4f0
Barzin <barzin.development@gmail.com>
2022-11-11 17:54:11 +0100

    React - Lesson 43 | React Todo App: Phase 7 -> Challenge: Style the completed todo items differently from the incomplete items.

--- my-todo-list/src/components/App.js
+++ my-todo-list/src/components/App.js
@@ -1,15 +1,6 @@
 /* Assignment description:
 
- * Let's make it so our checkbox can actually mark our todo as complete or incomplete!
- * This challenge is a little more involved than some of the past ones. Check the comments 
- * in the code for some help on accomplishing this one
- * 
- * Challenge: 
- * 1. Create an event handler in the App component for when the checkbox is clicked (which is an `onChange` event)
- *    a. This method will be the trickest part. Check the comments in the stubbed-out method below for some pseudocode to help guide you through this part
- * 2. Pass the method down to the TodoItem component
- * 3. In the TodoItem component, make it so when the `onChange` event happens, it calls the `handleChange` method and passes the id of the todo into the function
-
+* Challenge: Style the completed todo items differently from the incomplete items.
 
 */
 
@@ -51,12 +42,15 @@ class App extends Component {
 
 
     render() {
-        const TodoItems = this.state.todos.map(item => 
-        <TodoItem 
-            key={item.id} 
-            item={item} 
-            handleChange={this.handleChange}
-        />)
+        const TodoItems = this.state.todos.map(item => {
+            const completedStyle = item.completed ? "completed-task" : ""
+            return <TodoItem 
+                key={item.id} 
+                item={item} 
+                handleChange={this.handleChange}
+                styleCompleted = { completedStyle }
+            />
+        })
 
         return (
             <main className="main-wrapper">
--- my-todo-list/src/components/TodoItem.js
+++ my-todo-list/src/components/TodoItem.js
@@ -6,8 +6,7 @@ const TodoItem = (props) =>{
         <div style={{color:'rgba(12, 40, 78, 0.973)'}} className="todo-item">
             <input type="checkbox" checked={props.item.completed} 
                 onChange={()=> props.handleChange(props.item.id)}/>
-
-            <p>{props.item.text}</p>
+            <p className={props.styleCompleted}>{props.item.text}</p>
         </div>
 
     )
