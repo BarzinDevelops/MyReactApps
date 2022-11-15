182d45eeec8ff594c1c1c4072f05ce7bb3fb0dc1
Barzin <barzin.development@gmail.com>
2022-11-15 12:13:12 +0100

    Add input fields for first and last name. Make sure the form is controlled component where the element value is retrieved from state property value. Also create handleChange event handler so the value in input field get the state updated as you type in.

--- practice-forms/src/App.js
+++ practice-forms/src/App.js
@@ -1,11 +1,48 @@
 import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      hallo
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      firstName: "",
+      lastName: "",
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event){
+    const {name, value} = event.target
+    this.setState({
+      [name] : value
+
+    })
+  }
+
+  render(){
+    return (
+      <div className="main-div">
+         <input 
+            type="text" 
+            placeholder='First Name'
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.handleChange}
+          />
+         <input 
+            type="text" 
+            placeholder='Last Name'
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.handleChange}
+          />
+        <h3>{this.state.firstName} {this.state.lastName}</h3>
+      </div>
+    );
+  }
+
+
+  
 }
 
 export default App;
