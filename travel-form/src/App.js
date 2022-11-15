ef4d4576e30d095aa36c8492c6c6ca6839a96199
Barzin <barzin.development@gmail.com>
2022-11-15 23:59:05 +0100

    Create form fields for 'first name, last name and age'.

--- travel-form/src/App.js
+++ travel-form/src/App.js
@@ -1,11 +1,97 @@
+
+/**
+ * Challenge: Wire up the partially-finished travel form so that it works!
+ * Remember to use the concept of controlled forms
+ * https://reactjs.org/docs/forms.html
+ * 
+ * All information should be populating the text below the form in real-time
+ * as you're filling it out
+--------------------------------------------------------------------
+You just started your own airline, and you need to create a form to collect data about your passengers' upcoming travel plans so they can book their flight.
+
+You should collect the following information from the user:
+
+First name (text input)
+Last name (text input)
+Age (number input)
+Gender (radio buttons with 2 or more options)
+Location they're traveling to (select box with at least 3 options. You're an airline that doesn't fly to many places...)
+Whether they have any dietary restrictions (check boxes for vegetarian, kosher, lactose free, etc. Include at least 3 options)
+Each element of the form should be given a name attribute so you can access the data in JavaScript and do stuff with it.
+
+There should also be a button at the end of the form to submit it.
+ */
+
 import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Working?</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      firstName: "",
+      lastName: "",
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event){
+    const {name, value, type} = event.target;
+    
+    this.setState({
+      [name] : value
+    })
+  }
+  render(){
+    return (
+      <div className="main-wrapper">
+        <h1>Passenger Booking Form:</h1>
+        <form className="form-wrapper">
+            <input 
+              type="text" 
+              name="firstName" 
+              value={this.state.firstName} 
+              placeholder="First Name"
+              onChange={this.handleChange}
+            />
+            <input 
+              type="text" 
+              name="lastName" 
+              value={this.state.lastName} 
+              placeholder="Last Name"
+              onChange={this.handleChange}
+            />
+            {/* gender: */}
+            <label>
+              <input 
+                type="radio" 
+                name="gender" 
+                value={this.state.gender} 
+                onChange={this.handleChange}
+              />
+              Male
+            </label>
+            <label>
+              <input 
+                type="radio" 
+                name="gender" 
+                value={this.state.gender} 
+                onChange={this.handleChange}
+              />
+              Female
+            </label>
+        </form>
+        <fieldset className='results'>
+          <legend>Results of filled form:</legend>
+
+          <span>{this.state.firstName}</span>
+          <span>{this.state.lastName}</span>
+        </fieldset>
+  
+  
+      </div>
+    );
+  }
+
 }
 
 export default App;
