5c1fed0bea75ac0e74fab74e02cab0345e2c1bdf
Barzin <barzin.development@gmail.com>
2022-11-15 16:44:35 +0100

    Add checkbox input field. Also add text that indicates that state of checkbox gets changed based on checked propertie that is active/deactive. Change styling so that UI looks readable and user friendly.

--- practice-forms/src/App.js
+++ practice-forms/src/App.js
@@ -6,20 +6,26 @@ class App extends React.Component {
     this.state = {
       firstName: "",
       lastName: "",
+      comments: "",
+      isFriendly: false,
+      isFriendlyText: ""
     }
 
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event){
-    const {name, value} = event.target
-    this.setState({
-      [name] : value
-
-    })
+    const {name, value, type, checked} = event.target
+    type === "checkbox" ?   
+              this.setState({
+                [name] :  checked,
+                isFriendlyText: checked ?  "I seem to be a friendly person!" : "I seem to be an unfriendly person!"
+              }) :  
+              this.setState({ [name] :  value })
   }
 
   render(){
+    // console.log(this.state.isFriendly)
     return (
       <div className="main-div">
          <input 
@@ -36,7 +42,21 @@ class App extends React.Component {
             value={this.state.lastName}
             onChange={this.handleChange}
           />
-        <h3>{this.state.firstName} {this.state.lastName}</h3>
+          <textarea name='comments' value={this.state.comments} onChange={this.handleChange} />
+
+          <div className='is-friendly'>
+            <input
+              type="checkbox"
+              name="isFriendly"
+              checked={this.state.isFriendly}
+              onChange={this.handleChange}
+            />
+            <span>Are you a friendly person?</span>
+          </div>
+        <hr style={{width: "40%", margin: "10px 0"}}/>
+        <span>Name: {this.state.firstName} {this.state.lastName} </span>
+        <span> {this.state.isFriendlyText}</span>
+        <span className='comments'>Comments:<br/>&emsp; {this.state.comments}</span>
       </div>
     );
   }
