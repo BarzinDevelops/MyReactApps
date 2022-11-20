3ebc411145d7cd05d92dbbe89f0bf969eac4f635
Barzin <barzin.development@gmail.com>
2022-11-20 16:54:53 +0100

    Install randomcolor package and import in the project to use. Refactor code and styling so when user clicks on the page area outside main-div, the backgroundColor changes randomly (using randomcolor library you imported and updating the state).

--- my-counter/src/App.js
+++ my-counter/src/App.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
+import randomColors from 'randomcolor'
 
 
 class App extends Component{
     constructor(){
         super()
         this.state = {
-            counter: 0        
+            counter: 0,
+            mainDivBgColor: randomColors()
         }
         
         this.addOne = this.addOne.bind(this);
         this.subtractOne = this.subtractOne.bind(this);
         this.resetter = this.resetter.bind(this);
+
     }
     
     
@@ -41,14 +44,21 @@ class App extends Component{
             if( currentValue < 0 && operatorType === 'min') return {backgroundColor:"red"}
         }
         return (
-            <div className="main-div">
-               <h1 style={coloring(this.state.counter)}>{this.state.counter}</h1>
-               <div style={{display: "flex", gap:"10px"}}>
-                    <button onClick={this.addOne} style={btnColoring(this.state.counter, "plus")}>Add 1</button> 
-                    <button onClick={this.subtractOne} style={btnColoring(this.state.counter, "min")}>Subtract 1</button> 
-                    <button onClick={this.resetter} >Reset</button> 
+            <main>
+                <div className="main-div">
+                <h1 style={coloring(this.state.counter)}>{this.state.counter}</h1>
+                <div style={{display: "flex", gap:"10px"}}>
+                        <button onClick={this.addOne} style={btnColoring(this.state.counter, "plus")}>Add 1</button> 
+                        <button onClick={this.subtractOne} style={btnColoring(this.state.counter, "min")}>Subtract 1</button> 
+                        <button onClick={this.resetter} >Reset</button> 
+                    </div>
                 </div>
-            </div>
+                <div className='color-div' style={{backgroundColor: `${this.state.mainDivBgColor}`}}
+                  onClick={()=> this.setState({ mainDivBgColor: randomColors()})}
+                >   
+                </div>
+            </main>
+            
         )
     }
 }
